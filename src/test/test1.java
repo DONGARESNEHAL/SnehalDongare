d345461a94a6f018424594edf147d976432628ee
Dell <Dell@DESKTOP-BU6PB6V>
2022-10-07 14:18:15 +0530

    commit code

--- src/test/test1.java
+++ src/test/test1.java
@@ -2,6 +2,63 @@ package test;
 
 public class test1 {
 public static void main(String[] args) {
-	System.out.println("Hii team!!");
+int x= 2; int y=5;
+x=x+y; // x=7;
+y= x-y; // y= 2;
+x= x-y; // x=5;
+System.out.println("x =" + x);
+System.out.println("y=" + y);
+
+
+int m=  56;
+System.out.println(m);
+
+}}
+class Done
+{
+public static void main(String[] args) {
+char ch= 'A';
+int charVa= (int)ch;
+System.out.println("Ascci value for A ->" + charVa);
+char xy = 'A'; 
+int xyval= (int)xy;
+System.out.println("Ascci value for A->"+ xyval);
+
+
+}	
+
 }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
